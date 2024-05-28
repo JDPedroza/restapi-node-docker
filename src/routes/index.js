1b4bfebc5f19f9871a2cf5c7d892a5a2ac36dcf0
JohanHomeOffice <johandavidg14@gmail.com>
2024-05-28 16:38:33 -0500

    final prueba

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "hello world" }));
+router.get("/", (req, res) => res.json({ message: "hello world in live" }));
 
 module.exports = router;
